a60457527278cf770cfadad7aa97101d62a9cf32
lilei <lilei03@renrenche.com>
2018-06-05 14:42:29 +0800

    :sparkles: reg

---
+++ js/reg.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: LeeRay
+ * @Date: 2018-06-05 14:39:49
+ * @Last Modified by:   LeeRay
+ * @Last Modified time: 2018-06-05 14:39:49
+ */
+
+export default {
+  phone: /^1[3-9]\d{9}$/, // https://github.com/airyland/vux/blob/v2/src/tools/validator/chinaMobile.js
+  email: /^[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)+$/i
+}
